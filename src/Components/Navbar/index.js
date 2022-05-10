635bf49f6b8b52f94308c2602e41645b263ab250
DORRA <105285628+DorraSol@users.noreply.github.com>
2022-05-10 23:46:37 +0200

    Update index.js

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,34 +1,31 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import './style.js'
-import {NavbarSection,Logo, Anchor, ListItem, LogoText, UlList } from './style.js'
+import React  from 'react';
+import { Link } from 'react-router-dom'
+import { NavbarSection , Logo , LogoText , UlList , ListItem , Anchor } from './style.js'
+
 
 const Navbar = () => {
     return (
         <NavbarSection>
             
-        <div className="container">
+            <div className="container">
+                
+                <Logo>
+                    <LogoText>Ultra Profile</LogoText>
+                </Logo>
+                
+                <UlList>
+                    <ListItem><Link className='anchor' to="/">Home</Link></ListItem>
+                    <ListItem><Anchor href="#">Work</Anchor></ListItem>
+                    <ListItem><Anchor href="#">Portfolio</Anchor></ListItem>
+                    <ListItem><Anchor href="#">Resume</Anchor></ListItem>
+                    <ListItem><Anchor href="#">About</Anchor></ListItem>
+                    <ListItem><Link className='anchor' to="/contact">Contact</Link></ListItem>
+                </UlList>
+                
+            </div>
             
-            <Logo>
-                <LogoText>Dorra Profile</LogoText>
-            </Logo>
-            
-            <UlList>
-                <ListItem><Link to="/">Home</Link></ListItem>
-                <ListItem><Anchor href="#">Work</Anchor></ListItem>
-                <ListItem><Anchor href="#">Portfolio</Anchor></ListItem>
-                <ListItem><Anchor href="#">Resume</Anchor></ListItem>
-                <ListItem><Anchor href="#">About</Anchor></ListItem>
-                <ListItem><Anchor href="#">SocialMedia</Anchor></ListItem>
-                <ListItem><Link to="./contact">Contact</Link></ListItem>
-            </UlList>
-            
-        </div>
-        
-    </NavbarSection>
-         
+        </NavbarSection>
     )
 }
 
-
-export default Navbar
+export default Navbar;
