107625870ecddbe598a5b65f934e6198c0580257
DORRA19 <dorra.soltani.sediri@gmail.com>
2022-05-10 16:15:24 +0200

    mise à jour

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import { BrowserRouter, Routes, Link , Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Contact from './Components/Contact';
 import Navbar from './Components/Navbar';
@@ -7,13 +7,13 @@ import Index from './Components/Index';
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <HashRouter>
          <Navbar/>
           <Routes>
                 <Route path='/' element= {<Index />} />
                 <Route path='/Contact' element= {<Contact />} />
           </Routes>
-      </BrowserRouter>
+      </HashRouter>
       
     );
   }
